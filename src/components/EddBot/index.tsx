6e5f736ad7d903a18db42de88bc935846c1d75e4
Edgar Benavides <edgarben27@gmail.com>
2024-06-05 13:37:31 -0600

    feat: Translate portfolio to spanish

--- src/components/EddBot/index.tsx
+++ src/components/EddBot/index.tsx
@@ -35,7 +35,7 @@ export default function EddBot() {
           minute: "numeric",
           hour12: true,
         }).format(new Date()),
-      })
+      }),
     );
 
     const { answer } = await sendQuestion(question);
@@ -51,7 +51,7 @@ export default function EddBot() {
           minute: "numeric",
           hour12: true,
         }).format(new Date()),
-      })
+      }),
     );
   }
 
@@ -59,7 +59,7 @@ export default function EddBot() {
     setMessages([
       {
         owner: "bot",
-        text: "Hi there! As Edgar Benavides' personal assistant, I'm here to answer any questions you may have about him. Feel free to ask away!",
+        text: "Hola! como asistente personal de Edgar Benavides. Estoy aqui para responder todas tus preguntas que tengas acerca de su experiencia y trabajo.",
         time: new Intl.DateTimeFormat("es-MX", {
           hour: "numeric",
           minute: "numeric",
@@ -70,29 +70,31 @@ export default function EddBot() {
   }, []);
 
   useEffect(() => {
-    if(messages.length > 1) {
-      ref.current?.scrollIntoView({ behavior: "smooth", block: "nearest"});
+    if (messages.length > 1) {
+      ref.current?.scrollIntoView({ behavior: "smooth", block: "nearest" });
     }
-  }, [messages.length])
+  }, [messages.length]);
 
   return (
     <section className="space-y-10">
-      <div className="h-[320px] overflow-auto space-y-5">        
+      <div className="h-[320px] overflow-auto space-y-5">
         {messages.map((msg, index) => (
           <Message key={index} {...msg} />
         ))}
         {loading && (
-          <Message {...{
-            owner: "bot",
-            text: "Typing..."
-          }} />
+          <Message
+            {...{
+              owner: "bot",
+              text: "Escribiendo...",
+            }}
+          />
         )}
         <div ref={ref} />
       </div>
       <form onSubmit={handleSubmit} className="w-full flex gap-2">
         <input
           type="text"
-          placeholder="Ask me anything!"
+          placeholder="Escribe tu pregunta"
           name="question"
           disabled={loading}
           className="bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 focus:bg-black/10 dark:focus:bg-white/10 rounded-full px-4 py-2 focus:outline-none w-full"
@@ -101,8 +103,7 @@ export default function EddBot() {
           type="submit"
           disabled={loading}
           aria-label="Send question"
-          className="rounded-full bg-sky-300 dark:bg-sky-100 hover:bg-sky-400 hover:dark:bg-sky-200 text-sky-900 dark:text-sky-800 px-1.5 py-1 flex items-center justify-center"
-        >
+          className="rounded-full bg-sky-300 dark:bg-sky-100 hover:bg-sky-400 hover:dark:bg-sky-200 text-sky-900 dark:text-sky-800 px-1.5 py-1 flex items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -113,19 +114,10 @@ export default function EddBot() {
             fill="none"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="size-7"
-          >
-            <path
-              stroke="none"
-              d="M0 0h24v24H0z"
-              fill="none"
-            />
-            <path
-              d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z"
-            />
-            <path
-              d="M6.5 12h14.5"
-            />
+            className="size-7">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z" />
+            <path d="M6.5 12h14.5" />
           </svg>
         </button>
       </form>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,96 +1,111 @@
 export const LINKS = [
   {
-    name: 'Home',
-    href: '/',
-    enabled: false
+    name: "Inicio",
+    href: "/",
+    enabled: false,
   },
   {
-    name: 'About',
-    href: '#about',
-    enabled: true
+    name: "Acerca",
+    href: "#about",
+    enabled: true,
   },
   {
-    name: 'EddBot',
-    href: '#eddbot',
-    enabled: true
+    name: "EddBot",
+    href: "#eddbot",
+    enabled: true,
   },
   {
-    name: 'Experience',
-    href: '#experience',
-    enabled: true
+    name: "Experiencia",
+    href: "#experience",
+    enabled: true,
   },
   {
-    name: 'Projects',
-    href: '#projects',
-    enabled: true
+    name: "Proyectos",
+    href: "#projects",
+    enabled: true,
   },
   {
-    name: 'Contact',
-    href: '#contact',
-    enabled: false
-  }
-]
+    name: "Contacto",
+    href: "#contact",
+    enabled: false,
+  },
+];
 
 export const EXPERIENCE = [
   {
-    date: 'August 2023 - Actually',
-    title: 'Full Stack Developer - Magno Technology',
+    date: "Agosto 2023 - Actualmente",
+    title: "Desarrollador de Software - Magno Technology",
     responsabilities: [
       {
         id: 1,
-        description: "Develop and maintain server-side logic, manage databases, and ensure efficient communication between the frontend and backend."
+        description:
+          "Desarrollé la lógica del lado del servidor, gestioné bases de datos y garantizé una comunicación eficaz entre el frontend y el backend.",
       },
       {
         id: 2,
-        description: "Collaborate with designers and other developers to ensure a consistent and high-quality user experience."
+        description:
+          "Colaboré con diseñadores y otros desarrolladores para garantizar una experiencia de usuario coherente y de alta calidad.",
       },
       {
         id: 3,
-        description: "Design and implement attractive and functional user interfaces using technologies such as HTML, CSS and JavaScript."
+        description:
+          "Diseñe e implemente interfaces de usuario atractivas y funcionales utilizando tecnologías como HTML, CSS y JavaScript.",
       },
       {
         id: 4,
-        description: "Implement code optimization practices and regularly conduct performance testing."
+        description:
+          "Implementé prácticas de optimización del código y realicé periódicamente pruebas de rendimiento.",
       },
       {
         id: 5,
-        description: "Integrate external services and APIs to enhance application functionality and interoperability."
-      }
+        description:
+          "Integre servicios externos y API para mejorar la funcionalidad y la interoperabilidad de las aplicaciones.",
+      },
     ],
     link: null,
   },
   {
-    date: 'January 2023 - August 2023',
-    title: 'Web Developer - Freelancer',
+    date: "Enero 2023 - Agosto 2023",
+    title: "Desarrollador de Software - Freelancer",
     responsabilities: [
       {
         id: 1,
-        description: "Built landing pages for clients, which allowed them to boost their business, increasing their sales."
-      }
-    ],
-    link: null,
-  },
-  {
-    date: 'August 2021 - January 2023',
-    title: 'Web Developer - Bitmart',
-    responsabilities: [
-      {
-        id: 1,
-        description: "Design and implement the REST API for information consumption allowing to develop various web applications and saving time."
+        description:
+          "Construí páginas de aterrizaje para clientes, lo que les permitió impulsar su negocio, aumentando sus ventas.",
       },
       {
         id: 2,
-        description: "Develop the main internal system of the company that allows to manage customer information, implementing automations to save time to the users that use it."
+        description:
+          "Diseñé e implementé aplicaciones web a la medida permitiendo solucionar problemas de negocio de mis clientes.",
+      },
+    ],
+    link: null,
+  },
+  {
+    date: "Agosto 2021 - Enero 2023",
+    title: "Desarrollador de Software - Bitmart",
+    responsabilities: [
+      {
+        id: 1,
+        description:
+          "Diseñé e implementé la API REST para el consumo de información permitiendo desarrollar diversas aplicaciones web y ahorrando tiempo.",
+      },
+      {
+        id: 2,
+        description:
+          "Desarrollé el sistema interno principal de la empresa que permitió gestionar la información de los clientes, implementando automatizaciones para ahorrar tiempo a los usuarios que lo utilicen.",
       },
       {
         id: 3,
-        description: "Implement and manage containers that facilitate the deployment of applications that are developed."
+        description:
+          "Implementé y gestioné contenedores que facilitaron el despliegue de las aplicaciones que se desarrollaban.",
       },
       {
         id: 4,
-        description: "Help to have the highest availability of services and applications, managing servers and hosted databases."
-      }
+        description:
+          "Implementé estrategias para optimizar y garantizar la mayor disponibilidad de servicios y aplicaciones, gestionando servidores y bases de datos alojadas.",
+      },
     ],
     link: null,
-  }
-]
+  },
+];
